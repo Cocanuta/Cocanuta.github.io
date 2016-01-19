aa660e5009115e1551068d484700e94798e670eb
Ben Cherrington <Cocanuta@gmail.com>
2016-01-19 14:27:55 +0000

    Added cookies

--- xmlReader.js
+++ xmlReader.js
@@ -15,18 +15,15 @@ xhttp.send();
 
 function toggleCheckbox(checkbox, item)
 {
-    console.log("Event Fired");
     if(checkbox.checked)
     {
-        console.log("Checked");
         subtractFromRuntime(parseInt(item["Runtime"].data));
-        console.log(parseInt(item["Runtime"].data) + " removed from overallTime")
+        setCookie(item["Name"].data, checkbox.checked? 1: 0, 100);
     }
     if(!checkbox.checked)
     {
-        console.log("Unchecked");
         addToRuntime(parseInt(item["Runtime"].data));
-        console.log(parseInt(item["Runtime"].data) + " added to overallTime")
+        setCookie(item["Name"].data, checkbox.checked? 1: 0, 100);
     }
     updateTotal();
 }
@@ -98,6 +95,29 @@ function CountDownTimer(dt, id)
     timer = setInterval(showRemaining, 60000);
 }
 
+function setCookie(c_name, value, expiredays)
+{
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
+    document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : "; expires="+exdate);
+}
+
+function getCookie(c_name)
+{
+    if(document.cookie.length>0)
+    {
+        c_start = document.cookie.indexOf(c_name + "=")
+        if(c_start != 1)
+        {
+            c_start = c_start + c_name.length+1;
+            c_end=document.cookie.indexOf(";", c_start);
+            if(c_end==-1) c_end=document.cookie.lenght
+            return unescape(document.cookie.substring(c_start, c_end))
+        }
+    }
+    return null;
+}
+
 function initialize() {
     var container = document.getElementById("container");
     CountDownTimer('04/29/2016 00:01 AM', 'civilwarClock');
@@ -128,6 +148,7 @@ function initialize() {
         checkbox.type = "checkbox";
         checkbox.onclick = function(){toggleCheckbox(this, item)};
         checkbox.className = "css-checkbox";
+        checkbox.checked = getCookie(name)==1? true : false;
         checkbox.id = name;
 
         label.htmlFor = name;
@@ -138,7 +159,10 @@ function initialize() {
         newItem.appendChild(label);
 
         container.appendChild(newItem);
-        addToRuntime(runtime);
+        if(!checkbox.checked)
+        {
+            addToRuntime(runtime);
+        }
 
     });
     updateTotal();
